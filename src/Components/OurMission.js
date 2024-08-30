21997acd5953e41520d252b825adef757fd0fca1
Candy <obiijeoma32@gmail.com>
2024-08-30 08:41:01 +0100

    updated

--- src/Components/OurMission.js
+++ src/Components/OurMission.js
@@ -9,13 +9,13 @@ function OurMission() {
           <h3 className="w-full text-center text-neutral-700 100:text-[30px] 100:leading-[40px] md:text-[48px] md:leading-[60px] font-medium ">
             To be among the topmost providers of socio educational services, leveraging on technology, making academic consulting and advocacy available to its clients and promoting a culture of excellence
           </h3>
-          <section className=" flex flex-col gap-[16px] justify-center items-center">
+          {/* <section className=" flex flex-col gap-[16px] justify-center items-center">
             <img className=" w-[64px] rounded-full " src={lead} alt="Lead Consultant" />
             <div className=" flex-col justify-start items-start gap-1 inline-flex">
               <h4 className="self-stretch text-center text-gray-900 text-lg font-medium  leading-7">Femi Oke</h4>
               <p className="self-stretch text-center text-gray-500 text-base font-normal  leading-normal">Lead Consultant</p>
             </div>
-          </section>
+          </section> */}
         </section>
       </div>
     </>
